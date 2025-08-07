99403b257d328732052171b3337a98820c1e1288
Vitalii Kuzin <saynschuman@gmail.com>
2025-08-07 15:49:41 +0300

    refactor(telescope): remove commit action

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,22 +5,6 @@ return {
 
     local ns = vim.api.nvim_create_namespace("telescope_git_status")
 
-    local function commit(prompt_bufnr)
-      actions.close(prompt_bufnr)
-      vim.ui.input({ prompt = "Commit message: " }, function(input)
-        if not input or input == "" then
-          vim.notify("Commit aborted", vim.log.levels.WARN)
-          return
-        end
-        vim.fn.system({ "git", "commit", "-m", input })
-        if vim.v.shell_error ~= 0 then
-          vim.notify("Commit failed", vim.log.levels.ERROR)
-        else
-          vim.notify("Committed", vim.log.levels.INFO)
-        end
-      end)
-    end
-
     local function stage_toggle(prompt_bufnr)
       local state = require("telescope.actions.state")
       local entry = state.get_selected_entry()
@@ -40,7 +24,6 @@ return {
     for _, mode in ipairs({ "i", "n" }) do
       opts.pickers.git_status.mappings[mode] = opts.pickers.git_status.mappings[mode] or {}
       opts.pickers.git_status.mappings[mode]["<space>"] = stage_toggle
-      opts.pickers.git_status.mappings[mode]["<CR>"] = commit
     end
 
     return opts
