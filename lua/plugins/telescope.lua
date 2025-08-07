9cb3be2cc9ceb62167f0d9b73746f0f93d3b8852
Vitalii Kuzin <saynschuman@gmail.com>
2025-08-07 15:14:49 +0300

    feat(telescope): stage files and commit

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,35 +3,33 @@ return {
   opts = function(_, opts)
     local actions = require("telescope.actions")
     local action_state = require("telescope.actions.state")
-    local function commit_from_status(prompt_bufnr)
+
+    local function add_file(prompt_bufnr)
+      local selection = action_state.get_selected_entry()
       actions.close(prompt_bufnr)
-      vim.ui.input({ prompt = "Commit message: " }, function(input)
-        if not input or input == "" then
-          vim.notify("Commit aborted", vim.log.levels.WARN)
-          return
-        end
-        vim.fn.system({ "git", "commit", "-m", input })
-        if vim.v.shell_error ~= 0 then
-          vim.notify("Commit failed", vim.log.levels.ERROR)
-        else
-          vim.notify("Committed", vim.log.levels.INFO)
-        end
-      end)
+      local file = selection.path or selection.filename or selection.value
+      vim.fn.system({ "git", "add", file })
+      if vim.v.shell_error ~= 0 then
+        vim.notify("Add failed", vim.log.levels.ERROR)
+      else
+        vim.notify("Added " .. file, vim.log.levels.INFO)
+      end
     end
+
     opts.pickers = opts.pickers or {}
-    opts.pickers.git_status = opts.pickers.git_status or {}
-    opts.pickers.git_status.mappings = opts.pickers.git_status.mappings or {}
+    opts.pickers.find_files = opts.pickers.find_files or {}
+    opts.pickers.find_files.mappings = opts.pickers.find_files.mappings or {}
     for _, mode in ipairs({ "i", "n" }) do
-      opts.pickers.git_status.mappings[mode] = opts.pickers.git_status.mappings[mode] or {}
+      opts.pickers.find_files.mappings[mode] = opts.pickers.find_files.mappings[mode] or {}
     end
-    opts.pickers.git_status.mappings.i["<C-c>"] = commit_from_status
-    opts.pickers.git_status.mappings.n.c = commit_from_status
+    opts.pickers.find_files.mappings.i["<C-a>"] = add_file
+    opts.pickers.find_files.mappings.n.a = add_file
+
     return opts
   end,
   keys = {
-    { "<leader>gs", function() require("telescope.builtin").git_status() end, desc = "Git status" },
+    { "<leader>ga", function() require("telescope.builtin").find_files() end, desc = "Git add file" },
     { "<leader>gc", function() require("telescope.builtin").git_commits() end, desc = "Git commits" },
-    { "<leader>gb", function() require("telescope.builtin").git_branches() end, desc = "Git branches" },
     {
       "<leader>gC",
       function()
