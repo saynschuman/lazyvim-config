667af5f751fd137ce7650863bacc764b5ac104ca
Vitalii Kuzin <saynschuman@gmail.com>
2025-08-09 18:09:55 +0300

    onenord

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,97 +1,41 @@
 return {
-  "vague2k/vague.nvim",
-  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other plugins
-  config = function()
-    -- NOTE: you do not need to call setup if you don't want to.
-    require("vague").setup({
-      -- optional configuration here
-      transparent = true,
-      -- disable bold/italic globally in `style`
-      bold = true,
-      italic = true,
-      style = {
-        -- "none" is the same thing as default. But "italic" and "bold" are also valid options
-        boolean = "bold",
-        number = "none",
-        float = "none",
-        error = "bold",
-        comments = "italic",
-        conditionals = "none",
-        functions = "none",
-        headings = "bold",
-        operators = "none",
-        strings = "italic",
-        variables = "none",
+  {
+    "rmehri01/onenord.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('onenord').setup({
+        theme = nil,     -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
+        borders = true,  -- Split window borders
+        fade_nc = false, -- Fade non-current windows, making them more distinguishable
+        -- Style that is applied to various groups: see `highlight-args` for options
+        styles = {
+          comments = "NONE",
+          strings = "NONE",
+          keywords = "NONE",
+          functions = "NONE",
+          variables = "NONE",
+          diagnostics = "underline",
+        },
+        disable = {
+          background = false,       -- Disable setting the background color
+          float_background = false, -- Disable setting the background color for floating windows
+          cursorline = false,       -- Disable the cursorline
+          eob_lines = true,         -- Hide the end-of-buffer lines
+        },
+        -- Inverse highlight for different groups
+        inverse = {
+          match_paren = false,
+        },
+        custom_highlights = {}, -- Overwrite default highlight groups
+        custom_colors = {},     -- Overwrite default colors
+      })
+    end,
+  },
 
-        -- keywords
-        keywords = "none",
-        keyword_return = "italic",
-        keywords_loop = "none",
-        keywords_label = "none",
-        keywords_exception = "none",
-
-        -- builtin
-        builtin_constants = "bold",
-        builtin_functions = "none",
-        builtin_types = "bold",
-        builtin_variables = "none",
-      },
-      -- plugin styles where applicable
-      -- make an issue/pr if you'd like to see more styling options!
-      plugins = {
-        cmp = {
-          match = "bold",
-          match_fuzzy = "bold",
-        },
-        dashboard = {
-          footer = "italic",
-        },
-        lsp = {
-          diagnostic_error = "bold",
-          diagnostic_hint = "none",
-          diagnostic_info = "italic",
-          diagnostic_ok = "none",
-          diagnostic_warn = "bold",
-        },
-        neotest = {
-          focused = "bold",
-          adapter_name = "bold",
-        },
-        telescope = {
-          match = "bold",
-        },
-      },
-
-      -- Override highlights or add new highlights
-      on_highlights = function(highlights, colors) end,
-
-      -- Override colors
-      colors = {
-        bg = "#141415",
-        fg = "#cdcdcd",
-        floatBorder = "#878787",
-        line = "#252530",
-        comment = "#606079",
-        builtin = "#b4d4cf",
-        func = "#c48282",
-        string = "#e8b589",
-        number = "#e0a363",
-        property = "#c3c3d5",
-        constant = "#aeaed1",
-        parameter = "#bb9dbd",
-        visual = "#333738",
-        error = "#d8647e",
-        warning = "#f3be7c",
-        hint = "#7e98e8",
-        operator = "#90a0b5",
-        keyword = "#6e94b2",
-        type = "#9bb4bc",
-        search = "#405065",
-        plus = "#7fa563",
-        delta = "#f3be7c",
-      },
-    })
-    vim.cmd("colorscheme vague")
-  end
+  -- сообщаем LazyVim, какую тему используем (не обязательно, но удобно)
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "onenord" },
+  },
 }
