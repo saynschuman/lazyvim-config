a946867a09a0424f6a0856d9302ebf59f16530f7
Vitalii Kuzin <saynschuman@gmail.com>
2025-08-15 17:48:06 +0300

    ✨ feat(minimap): add minimap plugin integration
    Introduces the minimap.vim plugin to enhance code navigation in Neovim. This integration includes commands for toggling and managing the minimap, as well as keybindings for quick access. The plugin is configured to start automatically and highlights search results and ranges, improving the overall user experience.

---
+++ lua/plugins/minimap.lua
@@ -0,0 +1,18 @@
+-- ~/.config/nvim/lua/plugins/minimap.lua
+return {
+  {
+    "wfxr/minimap.vim",
+    build = "cargo install --locked code-minimap",
+    cmd = { "Minimap", "MinimapToggle", "MinimapClose", "MinimapRefresh", "MinimapUpdateHighlight", "MinimapRescan" },
+    keys = {
+      { "<leader>um", "<cmd>MinimapToggle<cr>", desc = "Toggle Minimap" },
+    },
+    init = function()
+      vim.g.minimap_width = 10
+      vim.g.minimap_highlight_range = 1
+      vim.g.minimap_highlight_search = 1
+      vim.g.minimap_auto_start = 1
+      vim.g.minimap_auto_start_win_enter = 1
+    end,
+  },
+}
