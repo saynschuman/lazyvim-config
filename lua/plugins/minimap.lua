10f45e047c1b9f5df3965b348975b75051513c39
Vitalii Kuzin <saynschuman@gmail.com>
2025-08-20 15:03:13 +0300

    ✨ feat(keymaps): add keymap for quitting
    Added a new keymap to allow users to quit the editor using <space>q. This provides a more convenient and consistent way to exit the editor, aligning with other custom key mappings. Additionally, the minimap plugin configuration was updated to disable auto-start, giving users more control over when the minimap is displayed.

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -11,7 +11,7 @@ return {
       vim.g.minimap_width = 10
       vim.g.minimap_highlight_range = 1
       vim.g.minimap_highlight_search = 1
-      vim.g.minimap_auto_start = 1
+      vim.g.minimap_auto_start = 0
       vim.g.minimap_auto_start_win_enter = 1
     end,
   },
