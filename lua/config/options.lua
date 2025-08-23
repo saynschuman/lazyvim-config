d71a4dd2a31d65b3caa55ac1e5d1001a11c132bb
Vitalii Kuzin <saynschuman@gmail.com>
2025-08-23 14:04:04 +0300

    ✨ feat(config): enhance statusline with git branch and position highlights
    Added custom highlight groups for the Git branch and position in the statusline. This change improves the visual clarity of the statusline by using distinct colors for the branch name and line/column position, enhancing the user experience when navigating through files.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.statusline = "%{system('git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -d \"\\n\"')}"
+vim.cmd([[
+  hi GitBranch guifg=#89b4fa guibg=NONE
+  hi SLPos guifg=#a6e3a1 guibg=NONE
+]])
+
+vim.o.statusline =
+    "%#GitBranch# %{system('git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -d \"\\n\"')} "
+    .. "%=%#SLPos#%l:%c"
